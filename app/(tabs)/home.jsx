aed5b5e8f36da628fe41d80c036e4dbcda82632d
machiloop <hamidusodiq2@gmail.com>
2025-03-09 04:12:21 +0100

    add appwrite authentication

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,17 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
